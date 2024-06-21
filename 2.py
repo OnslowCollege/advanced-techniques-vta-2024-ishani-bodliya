501133d8d1f5c61e9e801d414457cfe65912d44b
1sh4n1 <124644500+1sh4n1@users.noreply.github.com>
2024-06-21 02:34:45 +0000

    Jun 21, 2024, 2:34 AM

--- 2.py
+++ 2.py
@@ -18,17 +18,46 @@ while running:
     # Asks user which function they would like to perform.
     task = input("Choose an action (E, P, R): ")
 
+
     # If user picks task 2. Rules
     if task.upper() == "R":
         running = False
         print("--- RULES ---")
         # Prints Rules.
 
+
     # If user picks task 1. Play
     elif task.upper() == "P":
         running = False
         print("--- PLAY ---")
-        # Asks user for difficulty level they wish to proceed with.
+        # Keeps asking user for a difficulty until they type a valid input.
+        asking: bool = True
+        while asking:
+            # Asks user for difficulty level they wish to proceed with.
+            print(" ")
+            print("Please choose your difficulty")
+            print("-----------------------------")
+            print("   Easy    Medium    Hard   ")
+            print("-----------------------------")
+            # Sets the difficulty
+            difficulty = input("Type here: ")
+
+            # If user picks Easy
+            if difficulty.upper() == "EASY":
+                print("CHOSEN DIFFICULTY: EASY")
+            
+            # If user picks Medium
+            elif difficulty.upper() == "MEDIUM":
+                print("CHOSEN DIFFICULTY: MEDIUM")
+
+            # If user picks Hard
+            elif difficulty.upper() == "HARD":
+                print("CHOSEN DIFFICULTY: HARD")
+
+            # If input isn't valid
+            else:
+                print("That is an invalid option, try again.")
+
 
     # If user picks task 3. Exit
     elif task.upper() == "E":

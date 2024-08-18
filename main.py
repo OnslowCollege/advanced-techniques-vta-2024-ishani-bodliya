1338dc3c2de6e59508101aee3c7402cd4dafbc6a
1sh4n1 <124644500+1sh4n1@users.noreply.github.com>
2024-08-18 04:04:56 +0000

    Aug 18, 2024, 4:04 AM

--- main.py
+++ main.py
@@ -438,6 +438,7 @@ def ask_questions(topic, difficulty, question_num):
 
     # Display the question to the user.
     print(question_info["question"])
+    print("-------------")
     user_answer = input("Your answer: ").strip()
     answer = question_info["answer"].lower()
     # Compares user's answer with the correct answer, ignoring case and spaces.
